47b5946b2ee5c632df52397744861105c9b5a110
x-AZMAN-x <fromrimu@gmail.com>
2025-05-17 15:14:23 +0600

    acp for functions

---
+++ DemoFolder/functions in js acp/script.js
@@ -0,0 +1,17 @@
+//Functions
+function sumOftwoNums(n1,n2){
+    let multi = n1;
+    return multi;
+}
+//Calling Or Invoking A function
+document.getElementById("p1").innerHTML = multi(29);
+//Function Expression
+let multiplier = function(x){
+    return x+y;
+}
+document.getElementById("p2").innerHTML = multiplier(104)
+//Arrow Function
+let multiply = (o) =>o*1;
+document.writeln(multiply(8,4))
+
+document.getElementById.innerHTML = ("p1,p2");
