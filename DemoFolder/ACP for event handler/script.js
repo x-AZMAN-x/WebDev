05f1e27a3970690fd6dc41d2a63b55c6c0345066
x-AZMAN-x <fromrimu@gmail.com>
2025-05-24 16:22:22 +0600

    acp events handler

---
+++ DemoFolder/ACP for event handler/script.js
@@ -0,0 +1,9 @@
+function aFunction(){
+    document.getElementById("p1").innerHTML = `I Like Playing Games`;
+}
+function bFunction(){
+    document.getElementById("p2").innerHTML = `I Like Coding`;
+}
+function cFunction(){
+    document.getElementById("p3").innerHTML = `I Like Playing Outdoor Games`;
+}
