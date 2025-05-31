2d4bc2133197278471b118bc263197857e0b489d
x-AZMAN-x <fromrimu@gmail.com>
2025-05-31 17:54:42 +0600

    info about me about

---
+++ DemoFolder/about me and info/script.js
@@ -0,0 +1,3 @@
+function me(){
+    document.getElementById("myself").innerHTML = `My Name Is Azman. I Am 11 Years Old. I Study In Apple Tree International School(ATIS). My Favorite Sunbject Is Mathematics. I Love To Code. I Also Love Playing Games. I Have One Older Sister. And That's All About Me!`
+}
