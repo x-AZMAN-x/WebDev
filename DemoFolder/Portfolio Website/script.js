31ba6e7f60a9596b9ccec8cfb995f0c0e383f714
x-AZMAN-x <fromrimu@gmail.com>
2025-05-31 12:52:56 +0600

    cat dog info

---
+++ DemoFolder/Portfolio Website/script.js
@@ -0,0 +1,7 @@
+function cat(){
+    document.getElementById("cat-info").innerHTML = `The cat, also referred to as the domestic cat or house cat, is a small domesticated carnivorous mammal. It is the only domesticated species of the family Felidae. Advances in archaeology and genetics have shown that the domestication of the cat occurred in the Near East around 7500 BC.`;
+}
+const doggy = `The dog is a domesticated descendant of the gray wolf. Also called the domestic dog, it was selectively bred from an extinct population of wolves during the Late Pleistocene by hunter-gatherers. The dog was the first species to be domesticated by humans, over 14,000 years ago and before the development of agriculture.`;
+function dog(){
+    document.getElementById("dog-info").innerHTML = doggy;
+}
